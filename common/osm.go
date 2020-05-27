7a8184ea12ecfe92850c65cb33da3e93ecb2493e
Rory Hughes <rory.hughes@leonardo-itg.com>
2020-05-27 20:32:40 +0100

    updates osm to work on buildings too

--- common/osm.go
+++ common/osm.go
@@ -59,21 +59,24 @@ func LoadOSM(path string, bounds Rectangle) (*Graph, error) {
 }
 
 type OSMOptions struct {
-	Verbose bool
-	EdgeWidths []map[int]float64
-	NoParking bool
-	NoTunnels bool
-	LayerEdges []map[int]bool
-	EdgeTags []map[int]map[string]string
-	NodeTags []map[int]map[string]string
-	OneWay bool
-	OnlyMotorways bool
-	MotorwayEdges []map[int]bool
-	TunnelEdges []map[int]bool
-	Bytes []byte
+	Verbose         bool
+	EdgeWidths      []map[int]float64
+	NoParking       bool
+	NoTunnels       bool
+	LayerEdges      []map[int]bool
+	EdgeTags        []map[int]map[string]string
+	NodeTags        []map[int]map[string]string
+	OneWay          bool
+	OnlyMotorways   bool
+	MotorwayEdges   []map[int]bool
+	TunnelEdges     []map[int]bool
+	Bytes           []byte
 	CustomBlacklist []string
 	CustomWhitelist []string
-	IncludeRailway bool
+	IncludeRailway  bool
+	BuildingEdges   []map[int]bool
+	BuildingCentres []map[int]bool
+	DoRoads         bool
 }
 
 /*func LoadOSMMultiple(path string, regions []Rectangle, options OSMOptions) ([]*Graph, error) {
@@ -276,7 +279,6 @@ type OSMOptions struct {
 	return graphs, nil
 }*/
 
-
 func DecodeOSM(path string, options OSMOptions, f func(v interface{})) error {
 	var d *osmpbf.Decoder
 	if options.Bytes == nil {
@@ -325,6 +327,7 @@ func LoadOSMMultiple2(path string, regions []Rectangle, options OSMOptions) ([]*
 	}
 	vertexRegionMap := make(map[int64][]int)
 
+	fmt.Printf("Starting grid index\n")
 	// binary grid index over cells that we are interested in for the regions
 	regionIndex := make(map[[2]int][]int)
 	for regionID, region := range regions {
@@ -343,16 +346,19 @@ func LoadOSMMultiple2(path string, regions []Rectangle, options OSMOptions) ([]*
 	//  1) collect vertices in the bounds
 	//  2) collect edges from the OSM ways
 
+	fmt.Printf("Starting regionIndex\n")
+
 	var count int64 = 0
 	vertexStartTime := time.Now()
 	err := DecodeOSM(path, options, func(v interface{}) {
+
 		switch v := v.(type) {
 		case *osmpbf.Node:
 			point := Point{v.Lon, v.Lat}
-			x, y := int(point.X * OSM_INDEX_SCALE), int(point.Y * OSM_INDEX_SCALE)
+			x, y := int(point.X*OSM_INDEX_SCALE), int(point.Y*OSM_INDEX_SCALE)
 
 			type RegionVertex struct {
-				Vertex *Node
+				Vertex   *Node
 				RegionID int
 			}
 			var nodeVertices []RegionVertex
@@ -363,7 +369,7 @@ func LoadOSMMultiple2(path string, regions []Rectangle, options OSMOptions) ([]*
 					vertexIDMaps[regionID][v.ID] = vertex
 					vertexRegionMap[v.ID] = append(vertexRegionMap[v.ID], regionID)
 					nodeVertices = append(nodeVertices, RegionVertex{
-						Vertex: vertex,
+						Vertex:   vertex,
 						RegionID: regionID,
 					})
 				}
@@ -374,8 +380,8 @@ func LoadOSMMultiple2(path string, regions []Rectangle, options OSMOptions) ([]*
 					options.NodeTags[rvertex.RegionID][rvertex.Vertex.ID] = v.Tags
 				}
 			}
-			if options.Verbose && count % 10000000 == 0 {
-				fmt.Printf("finished %dM vertices (%d/sec)\n", count / 1000000, count / int64(time.Now().Sub(vertexStartTime).Seconds() + 1))
+			if options.Verbose && count%10000000 == 0 {
+				fmt.Printf("finished %dM vertices (%d/sec)\n", count/1000000, count/int64(time.Now().Sub(vertexStartTime).Seconds()+1))
 			}
 		}
 	})
@@ -383,152 +389,214 @@ func LoadOSMMultiple2(path string, regions []Rectangle, options OSMOptions) ([]*
 		return nil, err
 	}
 
-	blacklist := HIGHWAY_BLACKLIST
-	asWhitelist := false
-	if options.CustomBlacklist != nil {
-		blacklist = options.CustomBlacklist
-	} else if options.CustomWhitelist != nil {
-		blacklist = options.CustomWhitelist
-		asWhitelist = true
-	}
+	if options.DoRoads == true {
+		fmt.Printf("Parsing roads...")
+		blacklist := HIGHWAY_BLACKLIST
+		asWhitelist := false
+		if options.CustomBlacklist != nil {
+			blacklist = options.CustomBlacklist
+		} else if options.CustomWhitelist != nil {
+			blacklist = options.CustomWhitelist
+			asWhitelist = true
+		}
 
-	count = 0
-	err = DecodeOSM(path, options, func(v interface{}) {
-		switch v := v.(type) {
-		case *osmpbf.Way:
-			highway, ok := v.Tags["highway"]
-			if !ok && options.IncludeRailway {
-				_, ok = v.Tags["railway"]
-			}
-			if !ok {
-				return
-			} else if len(v.NodeIDs) < 2 {
-				return
-			}
-			isBlacklisted := IsOSMBlacklistedWithList(highway, blacklist)
-			if (!asWhitelist && isBlacklisted) || (asWhitelist && !isBlacklisted) {
-				return
-			}
-
-			if options.NoParking {
-				if v.Tags["amenity"] == "parking" || v.Tags["service"] == "parking_aisle" {
+		count = 0
+		err = DecodeOSM(path, options, func(v interface{}) {
+			switch v := v.(type) {
+			case *osmpbf.Way:
+				highway, ok := v.Tags["highway"]
+				if !ok && options.IncludeRailway {
+					_, ok = v.Tags["railway"]
+				}
+				if !ok {
 					return
-				} else if v.Tags["service"] == "driveway" {
+				} else if len(v.NodeIDs) < 2 {
 					return
 				}
-			}
-			isTunnel := (len(v.Tags["layer"]) >= 2 && v.Tags["layer"][0] == '-') || v.Tags["tunnel"] == "yes"
-			if options.NoTunnels && isTunnel {
-				return
-			}
-			isMotorway := v.Tags["highway"] == "motorway" || v.Tags["highway"] == "trunk"
-			if options.OnlyMotorways && !isMotorway {
-				return
-			}
+				isBlacklisted := IsOSMBlacklistedWithList(highway, blacklist)
+				if (!asWhitelist && isBlacklisted) || (asWhitelist && !isBlacklisted) {
+					return
+				}
 
-			// determine oneway, 0 for no, 1 for forward, -1 for reverse
-			oneway := 0
-			if options.OneWay {
-				// (1) if oneway tag is set, use that exclusively
-				//     (note that this overrides (2) since some ways can have motorway but
-				//      use tag set to "no" to disable oneway)
-				// (2) based on other tags that are default oneway
-				if v.Tags["oneway"] != "" {
-					if v.Tags["oneway"] == "yes" || v.Tags["oneway"] == "1" {
-						oneway = 1
-					} else if v.Tags["oneway"] == "-1" {
-						oneway = -1
+				if options.NoParking {
+					if v.Tags["amenity"] == "parking" || v.Tags["service"] == "parking_aisle" {
+						return
+					} else if v.Tags["service"] == "driveway" {
+						return
 					}
-				} else if v.Tags["highway"] == "motorway" || v.Tags["junction"] == "roundabout" {
-					oneway = 1
+				}
+				isTunnel := (len(v.Tags["layer"]) >= 2 && v.Tags["layer"][0] == '-') || v.Tags["tunnel"] == "yes"
+				if options.NoTunnels && isTunnel {
+					return
+				}
+				isMotorway := v.Tags["highway"] == "motorway" || v.Tags["highway"] == "trunk"
+				if options.OnlyMotorways && !isMotorway {
+					return
+				}
+
+				// determine oneway, 0 for no, 1 for forward, -1 for reverse
+				oneway := 0
+				if options.OneWay {
+					// (1) if oneway tag is set, use that exclusively
+					//     (note that this overrides (2) since some ways can have motorway but
+					//      use tag set to "no" to disable oneway)
+					// (2) based on other tags that are default oneway
+					if v.Tags["oneway"] != "" {
+						if v.Tags["oneway"] == "yes" || v.Tags["oneway"] == "1" {
+							oneway = 1
+						} else if v.Tags["oneway"] == "-1" {
+							oneway = -1
+						}
+					} else if v.Tags["highway"] == "motorway" || v.Tags["junction"] == "roundabout" {
+						oneway = 1
+					}
+				}
+
+				type RegionEdge struct {
+					Edge     *Edge
+					RegionID int
+				}
+
+				var wayEdges []RegionEdge
+				var lastVertexID int64 = v.NodeIDs[0]
+				for _, vertexID := range v.NodeIDs[1:] {
+					for _, regionID := range vertexRegionMap[vertexID] {
+						node1 := vertexIDMaps[regionID][lastVertexID]
+						node2 := vertexIDMaps[regionID][vertexID]
+						if node1 != nil && node2 != nil {
+							if oneway == 0 {
+								edge := graphs[regionID].AddBidirectionalEdge(node1, node2)
+								wayEdges = append(
+									wayEdges,
+									RegionEdge{edge[0], regionID},
+									RegionEdge{edge[1], regionID},
+								)
+							} else if oneway == 1 {
+								edge := graphs[regionID].AddEdge(node1, node2)
+								wayEdges = append(wayEdges, RegionEdge{edge, regionID})
+							} else if oneway == -1 {
+								edge := graphs[regionID].AddEdge(node2, node1)
+								wayEdges = append(wayEdges, RegionEdge{edge, regionID})
+							} else {
+								panic(fmt.Errorf("invalid oneway %d", oneway))
+							}
+						}
+					}
+					lastVertexID = vertexID
+				}
+
+				if len(options.EdgeWidths) > 0 {
+					var width float64
+					if val, ok := v.Tags["lanes"]; ok {
+						lanes, _ := strconv.ParseFloat(strings.Split(val, ";")[0], 64)
+						if lanes == 1 {
+							width = 6.6
+						} else {
+							width = lanes * 3.7
+						}
+					} else if val, ok := v.Tags["width"]; ok {
+						width, _ = strconv.ParseFloat(strings.Fields(strings.Split(val, ";")[0])[0], 64)
+					} else {
+						width = 6.6
+					}
+					for _, redge := range wayEdges {
+						options.EdgeWidths[redge.RegionID][redge.Edge.ID] = width
+					}
+				}
+
+				if len(options.LayerEdges) > 0 && v.Tags["layer"] != "" {
+					for _, redge := range wayEdges {
+						options.LayerEdges[redge.RegionID][redge.Edge.ID] = true
+					}
+				}
+
+				if len(options.EdgeTags) > 0 {
+					for _, redge := range wayEdges {
+						options.EdgeTags[redge.RegionID][redge.Edge.ID] = v.Tags
+					}
+				}
+
+				if len(options.MotorwayEdges) > 0 && isMotorway {
+					for _, redge := range wayEdges {
+						options.MotorwayEdges[redge.RegionID][redge.Edge.ID] = true
+					}
+				}
+
+				if len(options.TunnelEdges) > 0 && isTunnel {
+					for _, redge := range wayEdges {
+						options.TunnelEdges[redge.RegionID][redge.Edge.ID] = true
+					}
+				}
+
+				count++
+				if options.Verbose && count%100000 == 0 {
+					fmt.Printf("finished %dK ways\n", count/1000)
 				}
 			}
+		})
+		if err != nil {
+			return nil, err
+		}
 
-			type RegionEdge struct {
-				Edge *Edge
-				RegionID int
-			}
+		return graphs, nil
+	} else {
+		fmt.Printf("Parsing Buildings")
+		count = 0
+		err = DecodeOSM(path, options, func(v interface{}) {
+			switch v := v.(type) {
+			case *osmpbf.Way:
+				_, ok := v.Tags["building"]
+				if !ok {
+					return
+				} else if len(v.NodeIDs) < 2 {
+					return
+				}
+				//fmt.Printf("%d\n", v.NodeIDs)
+				type RegionEdge struct {
+					Edge     *Edge
+					RegionID int
+				}
 
-			var wayEdges []RegionEdge
-			var lastVertexID int64 = v.NodeIDs[0]
-			for _, vertexID := range v.NodeIDs[1:] {
-				for _, regionID := range vertexRegionMap[vertexID] {
-					node1 := vertexIDMaps[regionID][lastVertexID]
-					node2 := vertexIDMaps[regionID][vertexID]
-					if node1 != nil && node2 != nil {
-						if oneway == 0 {
+				var wayEdges []RegionEdge
+				var lastVertexID int64 = v.NodeIDs[0]
+				for _, vertexID := range v.NodeIDs[1:] {
+					for _, regionID := range vertexRegionMap[vertexID] {
+						node1 := vertexIDMaps[regionID][lastVertexID]
+						node2 := vertexIDMaps[regionID][vertexID]
+						if node1 != nil && node2 != nil {
 							edge := graphs[regionID].AddBidirectionalEdge(node1, node2)
 							wayEdges = append(
 								wayEdges,
 								RegionEdge{edge[0], regionID},
 								RegionEdge{edge[1], regionID},
 							)
-						} else if oneway == 1 {
-							edge := graphs[regionID].AddEdge(node1, node2)
-							wayEdges = append(wayEdges, RegionEdge{edge, regionID})
-						} else if oneway == -1 {
-							edge := graphs[regionID].AddEdge(node2, node1)
-							wayEdges = append(wayEdges, RegionEdge{edge, regionID})
-						} else {
-							panic(fmt.Errorf("invalid oneway %d", oneway))
 						}
 					}
+					lastVertexID = vertexID
 				}
-				lastVertexID = vertexID
-			}
 
-			if len(options.EdgeWidths) > 0 {
-				var width float64
-				if val, ok := v.Tags["lanes"]; ok {
-					lanes, _ := strconv.ParseFloat(strings.Split(val, ";")[0], 64)
-					if lanes == 1 {
-						width = 6.6
-					} else {
-						width = lanes * 3.7
+				if len(options.BuildingCentres) > 0 {
+					for _, redge := range wayEdges {
+						options.BuildingCentres[redge.RegionID][redge.Edge.ID] = true
 					}
-				} else if val, ok := v.Tags["width"]; ok {
-					width, _ = strconv.ParseFloat(strings.Fields(strings.Split(val, ";")[0])[0], 64)
-				} else {
-					width = 6.6
 				}
-				for _, redge := range wayEdges {
-					options.EdgeWidths[redge.RegionID][redge.Edge.ID] = width
-				}
-			}
 
-			if len(options.LayerEdges) > 0 && v.Tags["layer"] != "" {
-				for _, redge := range wayEdges {
-					options.LayerEdges[redge.RegionID][redge.Edge.ID] = true
+				if len(options.BuildingEdges) > 0 {
+					for _, redge := range wayEdges {
+						options.BuildingEdges[redge.RegionID][redge.Edge.ID] = true
+					}
+				}
+
+				count++
+				if options.Verbose && count%100000 == 0 {
+					fmt.Printf("finished %dK ways\n", count/1000)
 				}
 			}
-
-			if len(options.EdgeTags) > 0 {
-				for _, redge := range wayEdges {
-					options.EdgeTags[redge.RegionID][redge.Edge.ID] = v.Tags
-				}
-			}
-
-			if len(options.MotorwayEdges) > 0 && isMotorway {
-				for _, redge := range wayEdges {
-					options.MotorwayEdges[redge.RegionID][redge.Edge.ID] = true
-				}
-			}
-
-			if len(options.TunnelEdges) > 0 && isTunnel {
-				for _, redge := range wayEdges {
-					options.TunnelEdges[redge.RegionID][redge.Edge.ID] = true
-				}
-			}
-
-			count++
-			if options.Verbose && count % 100000 == 0 {
-				fmt.Printf("finished %dK ways\n", count / 1000)
-			}
+		})
+		if err != nil {
+			return nil, err
 		}
-	})
-	if err != nil {
-		return nil, err
-	}
 
-	return graphs, nil
+		return graphs, nil
+	}
 }
